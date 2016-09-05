9f226ab9d3de32206de24a98dd07c708affefeb2
ScarZ <ScarZ@ScarZ>
2016-09-05 16:51:53 +0700

    select car order 05/09/59

--- car/pre_confirm.php
+++ car/pre_confirm.php
@@ -1,3 +1,4 @@
+<?php  include 'connection/connect.php'; ?>
 <div class="row">
     <div class="col-lg-12">
         <h1><font color='blue'>  บันทึกการอนุมัติใช้รถยนต์ </font></h1> 
@@ -14,14 +15,42 @@
                 <h3 class="panel-title"><font color='brown'>ตารางบันทึกการอนุมัติใช้รถยนต์</font></h3>
             </div>
             <div class="panel-body">
+                <form method="post" action="" enctype="multipart/form-data" class="navbar-form navbar-right">
+                    <div class="form-group">
+                <select name="month_id" id="month"  class="form-control"> 
+				<?php	$sql = mysqli_query($db,"SELECT month_id, month_name FROM month order by m_id");
+				 echo "<option value=''>--เลือกเดือน--</option>";
+				 while( $result = mysqli_fetch_assoc( $sql ) ){
+  				 echo "<option value='".$result['month_id']."' $selected>".$result['month_name']." </option>";
+				 } ?>
+			 </select>
+            </div> 
+                        <div class="form-group"> 
+                            <select name='year'  class="form-control">
+                                <option value=''>กรุณาเลือกปีงบประมาณ</option>
+                                <?php
+                                for ($i = 2559; $i <= 2565; $i++) {
+                                    echo "<option value='$i'>$i</option>";
+                                }
+                                ?>
+                            </select>
+                        </div>
+                        <button type="submit" class="btn btn-success">ตกลง</button>
+                       <?php if (!empty($_REQUEST['year'])) {?>
+                        <button type="submit" class="btn btn-danger">เคลียร์</button>   
+                       <?php }?>
+                    </form>
                 <?php
 
-                include 'connection/connect.php'; 
+               
                 
+               
 // สร้างฟังก์ชั่น สำหรับแสดงการแบ่งหน้า   
 function page_navigator($before_p,$plus_p,$total,$total_p,$chk_page){   
 	global $e_page;
 	global $querystr;
+        $year = $_REQUEST['year'];
+        $take_month=$_REQUEST['month_id'];
 	$urlfile="index.php?page=car/pre_confirm"; // ส่วนของไฟล์เรียกใช้งาน ด้วย ajax (ajax_dat.php)
 	$per_page=10;
 	$num_per_page=floor($chk_page/$per_page);
@@ -33,25 +62,83 @@ function page_navigator($before_p,$plus_p,$total,$total_p,$chk_page){
 	$pNext=($pNext>=$total_p)?$total_p-1:$pNext;		
 	$lt_page=$total_p-4;
 	if($chk_page>0){  
-		echo "<a  href='$urlfile&s_page=$pPrev".$querystr."' class='naviPN'>Prev</a>";
+		echo "<a  href='$urlfile&year=$year&month_id=$take_month&s_page=$pPrev".$querystr."' class='naviPN'>Prev</a>";
 	}
 	for($i=$total_start_p;$i<$total_end_p;$i++){  
 		$nClass=($chk_page==$i)?"class='selectPage'":"";
 		if($e_page*$i<=$total){
-		echo "<a href='$urlfile&s_page=$i".$querystr."' $nClass  >".intval($i+1)."</a> ";   
+		echo "<a href='$urlfile&year=$year&month_id=$take_month&s_page=$i".$querystr."' $nClass  >".intval($i+1)."</a> ";   
 		}
 	}		
 	if($chk_page<$total_p-1){
-		echo "<a href='$urlfile&s_page=$pNext".$querystr."'  class='naviPN'>Next</a>";
+		echo "<a href='$urlfile&year=$year&month_id=$take_month&s_page=$pNext".$querystr."'  class='naviPN'>Next</a>";
 	}
 }  
                 include 'option/function_date.php';
-if($date >= $bdate and $date <= $edate){
+               if (!empty($_POST['year']) or !empty($_GET['year'])) {
+                    $year = $_REQUEST['year'] - 543;
+                    $years = $year + 543;
+                    $sql_month = mysqli_query($db,"SELECT month_name FROM month where month_id='".$_REQUEST['month_id']."'");
+                    $month = mysqli_fetch_assoc( $sql_month );
+                    
+                    if($date >= $bdate and $date <= $edate){
+                $take_month=$_REQUEST['month'];                      
+               
+                             $y= $Yy;
+                             $Y= date("Y");
+                             $take_month1="$Y-$take_month-01";
+                             if($take_month=='4' or $take_month=='6' or $take_month=='9' or $take_month=='11'){
+                               $take_month2="$Y-$take_month-30";  
+                             }elseif ($take_month=='2') {
+                               $take_month2="$Y-$take_month-29"; 
+                            }else{
+                             $take_month2="$Y-$take_month-31";
+                            }
+                             $take_date1="$Y-10-01";
+                             $take_date2="$y-09-30";
+    }  else {
+                $take_month=$_REQUEST['month_id'];
+                
+                if($take_month=='1' or $take_month=='2' or $take_month=='3' or $take_month=='4' or $take_month=='5' or $take_month=='6' or $take_month=='7' or $take_month=='8' or $take_month=='9'){
+                 $this_year=$y;
+                 $ago_year=$Y;
+                  $take_month1="$this_year-$take_month-01";
+                   if($take_month=='4' or $take_month=='6' or $take_month=='9'){
+                               $take_month2="$this_year-$take_month-30";  
+                             }elseif ($take_month=='2') {
+                               $take_month2="$this_year-$take_month-29"; 
+                            }else{
+                             $take_month2="$this_year-$take_month-31";
+                            }
+                             $take_date1="$ago_year-10-01";
+                             $take_date2="$this_year-09-30";
+                }  elseif($take_month=='10' or $take_month=='11' or $take_month=='12') {
+                 $this_year=$y;
+                 $ago_year=$Y;
+                 $next_year=$Yy;
+                  $take_month1="$ago_year-$take_month-01";
+                   if($take_month=='11'){
+                               $take_month2="$ago_year-$take_month-30";  
+                             }else{
+                             $take_month2="$ago_year-$take_month-31";
+                            }
+                             $take_date1="$ago_year-10-01";
+                             $take_date2="$this_year-09-30";
+                }  else {
+                 $this_year=$y;
+                 $ago_year=$Y;   
+                }
+                                }  
+                $code_where="'$take_month1' and '$take_month2'";                
+                }else{
+                                    if($date >= $bdate and $date <= $edate){
     $this_year=$y;
     $next_year=$Yy;
 }  else {
     $this_year=$Y;
     $next_year=$y;
+                                }
+             $code_where="'$this_year-10-01' and '$next_year-09-30'";                   
 }
     $q="SELECT ssc . * , CONCAT( p1.pname, e1.firstname,  ' ', e1.lastname ) AS fullname, d1.depName AS dep, e1.empno AS empno,
                                  am.AMPHUR_NAME as amphur, pv.PROVINCE_NAME as province, ssc.passenger
@@ -61,7 +148,7 @@ INNER JOIN pcode p1 ON e1.pcode = p1.pcode
 INNER JOIN department d1 ON e1.depid = d1.depId
 INNER JOIN amphur am on am.AMPHUR_ID=ssc.amphur
 INNER JOIN province pv on pv.PROVINCE_ID=ssc.province
-where ssc.approve='Y' and ssc.start_date between '$this_year-10-01' and '$next_year-09-30'
+where ssc.approve='Y' and ssc.start_date between $code_where
 order by ssc.car_id desc";
     $qr=mysqli_query($db,$q);
 if($qr==''){exit();}
@@ -89,6 +176,10 @@ echo mysqli_error($db);
                     <?php include_once ('option/funcDateThai.php'); ?>
                     <table align="center" width="100%" class="table-responsive table-bordered table-hover">
                         <thead>
+                          <?php  if (!empty($_REQUEST['year'])) {?>
+                    <tr align="center">
+                        <td colspan="14"><b>ปีงบประมาณ <?= $years?>  ประจำเดือน <?= $month['month_name']?></b></td>
+                          </tr><?php }?>
                     <tr align="center" bgcolor="#898888">
                         <td width="3%" align="center"><b>ลำดับ</b></td>
                         <td width="8%" align="center"><b>เลขใบคำขอ</b></td>
